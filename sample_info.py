0ae2e461adf1b419cf9d1f128e1fec4962f27d55
djfaizp <modan.mohammad@gmail.com>
2022-08-24 14:42:46 -0700

    info.py

--- sample_info.py
+++ sample_info.py
@@ -1,23 +1,23 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = ''
+API_ID = 14158285
+API_HASH = '7480e42962f3e1b89a653f5c892cc4d5'
+BOT_TOKEN = '5600131836:AAEAZHH-PXPqsI-RjWkg5HbkRqtCf9fYv3Q'
+USERBOT_STRING_SESSION = '1BVtsOL8Bu2DNRGKwqYVGzAO4iaTitlpb3-9qDglK7ez336I3GhDPMx1JYO30vUsJVAZeJCgrMZcovlFMR4ngF6mdyBOJWzQQEJ_kPtcHwdc5AZdOyZZUQrzISL-TeBkydAlt3latsvC_QVtssXmgX4MrBujYhtd_q-Xhi8O-gpHCxogWtvYN1km-BNplVqyaVshARAGKb35JlfiJuLXyC4DOFUYnd30YHWigvmeQhB3h5kTgQ6bhGWFRrJnXXsp95mbzzdSSdwadr3D5GY_B3lsR3XDzFzby717Pof1sl4kBPdbnRuTRLJnK102BfaQK8LIS7N1dbaU21MR4_0ATnK0o2DJZb58='
 
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [5372143244]
+CHANNELS = [-1001684448178, -1001606250922, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://faiz:faiz@cluster0.bo0wi9t.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
